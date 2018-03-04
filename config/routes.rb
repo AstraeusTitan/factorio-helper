4df45fb4f421a7ad3dc3646b33139b7a02fb4713
Cody Constant <astraeusdev@gmail.com>
2018-03-03 20:39:17 -0800

    User signup now works

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get 'home', to: 'static_pages#home'
-
   get 'signup', to: 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'signup', to: 'users#create'
+  resources :users
 end
