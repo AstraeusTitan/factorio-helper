701bea11ed715225e7bfe6e5b86fe834a2542d92
Cody Constant <astraeusdev@gmail.com>
2018-03-06 23:31:25 -0800

    Created ingredients model
    Created ingredients model and added relation to recipe model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,6 @@
 class Ingredient < ApplicationRecord
+  validates :name, presence: true
+  validates :amount, numericality: { greater_than: 0 }, presence: true
+
+  belongs_to :recipe
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,13 @@ class Recipe < ApplicationRecord
   validates :energy_cost, numericality: { greater_than: 0.0 }, presence: true
 
   belongs_to :user
+  has_many :ingredients
+
+  def inputs
+    ingredients.select { |i| i.input? }
+  end
+
+  def outputs
+    ingredients.select { |i| !i.input? }
+  end
 end
