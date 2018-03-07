7eb924dc4b5513a6fe06b22d6c55eac9320b2b33
Cody Constant <astraeusdev@gmail.com>
2018-03-06 23:31:54 -0800

    Created ingredients tests

--- test/models/ingredient_test.rb
+++ test/models/ingredient_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class IngredientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @ingredient = Ingredient.new name:'valid name', amount: 2, input: false,
+      recipe: recipes(:magazine)
+  end
+
+  test 'ingredient should be valid' do
+    assert @ingredient.valid?
+  end
+
+  test 'name should be present' do
+    @ingredient.name = '  '
+    assert_not @ingredient.valid?
+  end
+
+  test 'amount should be a number' do
+    @ingredient.amount = 'not a number'
+    assert_not @ingredient.valid?
+  end
+
+  test 'amount should be greater than 0' do
+    @ingredient.amount = -1
+    assert_not @ingredient.valid?
+  end
+
+  test 'should tell if an input' do
+    assert_not @ingredient.input?
+    @ingredient.input = true
+    assert @ingredient.input?
+  end
 end
