25d58d0eb5a9e581fb59f93e4ef714aae524aacc
Cody Constant <astraeusdev@gmail.com>
2018-03-06 23:22:29 -0800

    Created ingredients table

--- db/migrate/20180307065944_create_ingredients.rb
+++ db/migrate/20180307065944_create_ingredients.rb
@@ -1,6 +1,11 @@
 class CreateIngredients < ActiveRecord::Migration[5.1]
   def change
     create_table :ingredients do |t|
+      t.string :name
+      t.integer :amount, default: 1
+      t.boolean :input, default: true
+
+      t.belongs_to :recipe, index: true
 
       t.timestamps
     end
