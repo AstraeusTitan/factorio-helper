eddd2751c1002443d652071f62dcbea073c2c863
Cody Constant <astraeusdev@gmail.com>
2018-03-06 22:58:30 -0800

    Updated tests
    Added tests for recipes, and updated tests for user

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class RecipeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @recipe = Recipe.new name:'good-name', energy_cost: 1.0, user: users(:bob)
+  end
+
+  test 'recipe should be valid' do
+    assert @recipe.valid?
+  end
+
+  test 'name should be present' do
+    @recipe.name = '  '
+    assert_not @recipe.valid?
+  end
+
+  test 'energy_cost should be a number' do
+    @recipe.energy_cost = 'not a number'
+    assert_not @recipe.valid?
+  end
+
+  test 'energy_cost should be greater than 0' do
+    @recipe.energy_cost = 0
+    assert_not @recipe.valid?
+  end
+
+  test 'should tell if modded' do
+    assert @recipe.modded?
+    @recipe.modded = false
+    assert_not @recipe.modded?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -20,7 +20,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "email validation should accept valid addresses" do
+  test 'email validation should accept valid addresses' do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
@@ -29,7 +29,7 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test "email validation should reject invalid addresses" do
+  test 'email validation should reject invalid addresses' do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
@@ -38,14 +38,14 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test "email addresses should be unique" do
+  test 'email addresses should be unique' do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
 
-  test "email addresses should be saved as lower-case" do
+  test 'email addresses should be saved as lower-case' do
     mixed_case_email = "Foo@ExAMPle.CoM"
     @user.email = mixed_case_email
     @user.save
@@ -57,14 +57,18 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "password should be present (nonblank)" do
+  test 'password should be present (nonblank)' do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
 
-  test "password should have a minimum length" do
+  test 'password should have a minimum length' do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
 
+  test 'user recipes should be accessible' do
+    user = users :bob
+    assert_not user.recipes.empty?
+  end
 end
