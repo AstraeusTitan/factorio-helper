0c2e92e4283ac48fa85098d67390bbdc157622e1
Cody Constant <astraeusdev@gmail.com>
2018-03-07 21:17:24 -0800

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   root 'static_pages#welcome'
-
+  # Static pages
   get '/welcome', to: 'static_pages#welcome'
+  # Users
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/account', to: 'users#show'
   get '/account/edit', to: 'users#edit'
+  # Sessions
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#delete'
+  # Recipes
   get '/browse', to: 'recipes#index'
-
-  resources :ingredients
-  resources :recipes
+  get '/recipe/:id', to: 'recipes#show', as: :recipe
 end
