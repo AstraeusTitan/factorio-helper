027446f0fe3f661043324454bccb2f7f7642a0fc
Cody Constant <astraeusdev@gmail.com>
2018-03-07 20:50:58 -0800

    Roughed out recipe controller
    Got a rough index and show action setup.
    Added a test for fetching the browse page.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,13 @@
 class RecipesController < ApplicationController
+  def index
+    unless logged_in?
+      @recipes = Recipe.base
+    else
+      @recipes = Recipe.modded.user current_user
+    end
+  end
+
+  def show
+    @recipe = Recipe.find params[ :id ]
+  end
 end
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get browse page' do
+    get browse_path
+    assert_response :success
+    log_in_as users :bob
+    get browse_path
+    assert_response :success
+  end
 end
