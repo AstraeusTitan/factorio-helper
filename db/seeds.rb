d1e05ba14d676e04484e0d0723f32a507384eee4
Cody Constant <astraeusdev@gmail.com>
2018-03-17 08:52:36 -0700

    Updated seeding script

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,29 +2,37 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 require 'json'
 
-Recipe.destroy_all
+# Seed admin
+User.destroy_all
+
+admin = User.new
+admin.email = 'astraeusdev@gmail.com'
+admin.password = User.digest('Qp7Al4Zm1')
+admin.save
+
+# Seed recipes with base recipes
+Recipe.owned_by(admin).destroy_all
 
 file = File.read(Rails.root.join('db', 'seeds', 'recipe_seed_data.json'))
 data = JSON.parse file
 
 puts data[ 'recipes' ].count
-data[ 'recipes' ].each do |recipe|
-  record = Recipe.new
-  record.user_id = 1  #easy to tell what data is seeded
-  record.name = recipe[ 'name' ]
-  record.energy_cost = recipe[ 'energy' ]
-  record.modded = false
+Recipe.transaction do
+  data[ 'recipes' ].each do |recipe|
+    record = Recipe.new
+    record.user = admin
+    record.name = recipe[ 'name' ]
+    record.energy_cost = recipe[ 'energy' ]
+    record.modded = false
+    record.save
 
-  recipe[ 'inputs' ].each do |name, value|
-    record.ingredients.build name: name, amount: value, input: true
-  end
+    recipe[ 'inputs' ].each do |name, value|
+      record.ingredients.create name: name, amount: value, input: true, recipe: record
+    end
 
-  recipe[ 'outputs' ].each do |name, value|
-    record.ingredients.build name: name, amount: value, input: false
+    recipe[ 'outputs' ].each do |name, value|
+      record.ingredients.create name: name, amount: value, input: false, recipe: record
+    end
   end
-  puts record
-  puts record.valid?
-  puts record.errors.full_messages
-  record.save
 end
 
